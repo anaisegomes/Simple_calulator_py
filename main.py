878609658f23b2718f59161032249fda8a0a7146
anaisegomes <naiise321@gmail.com>
2026-05-17 20:49:38 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Python Calculator
+
+operator = input("Enter an operator (+ - * /): ")
+
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+if operator == "+":
+    pass
+elif operator == "-":
+    pass
+elif operator == "*":
+    pass
+elif operator == "/":
+    pass
