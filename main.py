09f0dade917df13d49fb89a9994a4dab153154b6
anaisegomes <naiise321@gmail.com>
2026-05-19 21:50:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ num2 = float(input("Enter the 2nd number: "))
 if operator == "+":
     result = (num1 + num2)
     print(round(result))
-elif operator == "-":
+elif operator == "-": 
     result = (num1 - num2)
     print(round(result))
 elif operator == "*":
