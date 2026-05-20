423d31de514e238585c4015736987930091b121d
anaisegomes <naiise321@gmail.com>
2026-05-19 21:53:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st number: "))
 num2 = float(input("Enter the 2nd number: "))
 
-if operator == "+":
-    result = (num1 + num2)
-    print(round(result))
-elif operator == "-": 
-    result = (num1 - num2)
-    print(round(result))
-elif operator == "*":
-    result = (num1 * num2)
-    print(round(result))
-elif operator == "/":
-    result = (num1 / num2)
-    print(round(result))
+try: 
+    if operator == "+":
+        result = (num1 + num2)
+        print(round(result, 3))
+    elif operator == "-": 
+        result = (num1 - num2)
+        print(round(result, 3))
+    elif operator == "*":
+        result = (num1 * num2)
+        print(round(result, 3))
+    elif operator == "/":
+        result = (num1 / num2)
+        print(round(result, 3))
+
+except:
+        print(f"{operator} is not valid")
+
